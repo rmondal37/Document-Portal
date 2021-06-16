c6cf8b89c8712ee49b917802b553a028003cf7f7
Rishav mondal <58592749+rmondal37@users.noreply.github.com>
2021-06-16 21:21:04 +0530

    updated requirements.txt and added Procfile

--- docu_portal/docu_portal/settings.py
+++ docu_portal/docu_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,6 @@ STATICFILES_DIRS = [
 # for Media
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = 'home/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
